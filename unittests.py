bc7a933f705845ee02568038f70f343f5cc9dfba
dscer <86316864+dscer@users.noreply.github.com>
2022-04-13 18:42:25 -0400

    addded unit test

--- unittests.py
+++ unittests.py
@@ -1,6 +1,7 @@
 
 import os
 import unittest
+import pycountry
 import geopandas as gpd
 
 class GDFTest(unittest.TestCase):
@@ -22,5 +23,21 @@ class GDFTest(unittest.TestCase):
         sample_gdf = gpd.GeoDataFrame({'geometry': []}, crs="EPSG:4326")
         self.assertEqual(test_gdf['geometry'].dtype, sample_gdf['geometry'].dtype)
         
+    def test_country_info(self):
+        """
+        Unit test to check if the country codes and the names match as expected
+        
+        Returns
+        -------
+        None.
+
+        """
+        country_dat = {'Germany': 'DEU', 'United States': 'USA', 'Italy': 'ITA', 'Australia': 'AUS'}
+        
+        for country_name, country_code in country_dat.items():
+            result = pycountry.countries.get(alpha_3=country_code)
+            result.name
+            self.assertTrue(country_name==result.name, f'Expected name {country_name} does not match {result.name}')
+    
 if __name__ == '__main__':
     unittest.main()
